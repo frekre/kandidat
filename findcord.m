169e72c8dc21458d9de62b8c57953b132577f99d
josfri <108523699+josfri@users.noreply.github.com>
2023-04-27 10:39:06 +0200

    Update findcord.m
    begränsa tidsintervallet där vi letar efter max till 0-4 sekunder (av totala -2 till 7)

--- findcord.m
+++ findcord.m
@@ -1,8 +1,11 @@
 function [ZmaxCol, ZmaxRow, Zmax] = findcord(SPEC, realisation)
 %hjälpfunktion för att hittaindex i matris för x och y; 
 % zmaxcol --> x (tid ) zmaxrow --> frekvens y 
+Fs = 62.55556;
+tmin = round(2*Fs);
+tmax = round(6*Fs);
 
-Z= SPEC(:,:,realisation);
+Z= SPEC(:,tmin:tmax,realisation);
 Zmax = max(Z(:));
 
 [Zmax,Idx] = max(Z(:));
