71aa45948c32d1ee0d1304d1af967444df57f3bd
frekre <131352568+frekre@users.noreply.github.com>
2023-05-04 10:22:50 +0200

    baselinefunktion löst

--- baselinecorrection.m
+++ baselinecorrection.m
@@ -1,4 +1,4 @@
-function [baselinecorrectedchannel, sum] = baselinecorrection(data, channel, trial)
+function [baselinecorrectedchannel, baseline] = baselinecorrection(data, channel, trial)
 %BASELINECORRECTION Function that calculates the average energy of the
 %baseline activity and subtracts it from the signal. Does not downsample or filter the signal. 
 %data: which datasource that should be used.
@@ -9,14 +9,9 @@ function [baselinecorrectedchannel, sum] = baselinecorrection(data, channel, tri
 baselinecorrectedchannel = data.trial{1, trial}(channel, :);
 
 %hitta medelvärdet som ska dras bort (första två sekunderna, 1000 samples)
-sum = 0; 
-for i= 1:100
-    sum = sum + baselinecorrectedchannel(1,i);
-end
-sum = sum/1000; 
+baseline=mean(baselinecorrectedchannel(1,1:1000));
+baselinecorrectedchannel(1,:)=baselinecorrectedchannel(1,:)-baseline;
 
-%dra bort värdet från signalen: 
-baselinecorrectedchannel = baselinecorrectedchannel-sum;
 
 
 end
