463e7c2c79ba11681aae3ea3863585f19588e5a3
frekre <131352568+frekre@users.noreply.github.com>
2023-05-05 09:20:19 +0200

    fix findchannels
    innan fungerade den bara för 60 kanaler men vissa subjects har färre så jag skrev om den till generell längd.

--- findChannels.m
+++ findChannels.m
@@ -3,7 +3,7 @@ function [channel] = findChannels(data, name)
 %name. 
 channel = [];
 for a = 1:length(name)
-    for i=1:60
+    for i=1:length(data.cfg.channel)
         if isequal(data.cfg.channel{i, 1}, name{a})
             channel(end +1) = i;
         end
