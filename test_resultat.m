2d180d49138794ac67336b656cb149d24ecc9b74
josfri <108523699+josfri@users.noreply.github.com>
2023-05-11 15:21:37 +0200

    Update test_resultat.m

--- test_resultat.m
+++ test_resultat.m
@@ -1,10 +1,10 @@
 %kalla på resultat
 %innan: gå in i metod, fixa axlar + titel
 
-[vS1, hS1, vSRS1, hSRS1, vS2, hS2, vSRS2, hSRS2]=resultmaker(data, {'FC3'}, {'FC4'});
+[vS1, hS1, vSRS1, hSRS1, vS2, hS2, vSRS2, hSRS2]=resultmaker(data, {'C3'}, {'C4'});
 %%
 
-axparam = [0 8*10^6 0 11];
-titel = "subject 13";
+axparam = [0 2*10^6 0 13];
+titel = "subject 12 + lambda = 6";
 
 resultdrawer(vS1, hS1, vSRS1, hSRS1, vS2, hS2, vSRS2, hSRS2, axparam, titel);
