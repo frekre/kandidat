931517e293ff72ff8a724c23f5b1f8b035a25e54
josfri <108523699+josfri@users.noreply.github.com>
2023-05-11 12:48:15 +0200

    facealpha

--- josses_test_statistik.m
+++ josses_test_statistik.m
@@ -134,11 +134,13 @@ xlabel('energy');
 ylabel('occurence');
 h1(1).FaceColor = 'c';
 h1(2).Color = 'b';
+h1(1).FaceAlpha = 0.7;
 hold on
 h2 = histfit(hS1(:,4), 70, 'kernel');
 axis([0 2.5*10^6 0 18])
 h2(1).FaceColor = 'm';
 h2(2).Color = 'r';
+h2(1).FaceAlpha = 0.7;
 legend('vänster', 'vänster kernel', 'höger', 'höger kernel');
 subtitle('sida 1 trial och S');
 hold off
@@ -150,11 +152,13 @@ xlabel('energy');
 ylabel('occurence');
 h1(1).FaceColor = 'c';
 h1(2).Color = 'b';
+h1(1).FaceAlpha = 0.7;
 hold on
 h2 = histfit(hS2(:,4), 70, 'kernel');
 axis([0 2.5*10^6 0 18])
 h2(1).FaceColor = 'm';
 h2(2).Color = 'r';
+h2(1).FaceAlpha = 0.7;
 legend('vänster', 'vänster kernel', 'höger', 'höger kernel');
 subtitle('sida 2 trial och S');
 hold off
@@ -166,11 +170,13 @@ xlabel('energy');
 ylabel('occurence');
 h1(1).FaceColor = 'c';
 h1(2).Color = 'b';
+h1(1).FaceAlpha = 0.7;
 hold on
 h2 = histfit(hSRS1(:,4), 70, 'kernel');
 axis([0 2.5*10^6 0 18])
 h2(1).FaceColor = 'm';
 h2(2).Color = 'r';
+h2(1).FaceAlpha = 0.7;
 legend('vänster', 'vänster kernel', 'höger', 'höger kernel');
 subtitle('sida 1 trial och SRS');
 hold off
@@ -182,11 +188,13 @@ xlabel('energy');
 ylabel('occurence');
 h1(1).FaceColor = 'c';
 h1(2).Color = 'b';
+h1(1).FaceAlpha = 0.7;
 hold on
 h2 = histfit(hSRS2(:,4), 70, 'kernel');
 axis([0 2.5*10^6 0 18])
 h2(1).FaceColor = 'm';
 h2(2).Color = 'r';
+h2(1).FaceAlpha = 0.7;
 legend('vänster', 'vänster kernel', 'höger', 'höger kernel');
 subtitle('sida 2 trial och SRS');
 hold off
